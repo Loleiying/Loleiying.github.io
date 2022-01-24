cf06ac23573a01fb9123a7b7846b24760965a770
loleiying <liyinglo914@gmail.com>
2022-01-23 20:43:35 -0500

    changed to edu email

--- forms/contact.php
+++ forms/contact.php
@@ -7,7 +7,7 @@
   */
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'liyinglo914@gmail.com';
+  $receiving_email_address = 'llu21@gmu.edu';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
